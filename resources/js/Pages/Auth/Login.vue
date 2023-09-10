5cb607e41bc154eb51473d61a0be7818cf7b8236
hfukuyama <h.i.d.e.20070621@gmail.com>
2023-09-10 14:36:38 +0900

    商品関連

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -5,11 +5,14 @@ import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import BreezeValidationErrors from '@/Components/ValidationErrors.vue'
+import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
+
 
 defineProps({
     canResetPassword: Boolean,
     status: String,
+    errors: Object
 });
 
 const form = useForm({
@@ -28,6 +31,8 @@ const submit = () => {
 <template>
     <GuestLayout>
         <Head title="Log in" />
+
+        <BreezeValidationErrors class="mb-4" :errors="errors" />
 
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
             {{ status }}
